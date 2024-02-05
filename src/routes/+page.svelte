a57bdb4769ce3db9e8e57406fb16fcf618ffa528
Kyle Peavot <kyle.peavot@ebm.ai>
2024-02-05 15:09:36 +0000

    Refactor code for better readability and maintainability

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import P5, { type Sketch, type p5 } from 'p5-svelte';
-	import { Cell } from '../lib/index'
+	import P5, { type Sketch, type p5 } from "p5-svelte";
+	import { Cell } from "../lib/index";
 
 	/**
 	 * Any live cell with fewer than two live neighbors dies, as if by underpopulation.
@@ -8,23 +8,25 @@
 	 * Any live cell with more than three live neighbors dies, as if by overpopulation.
 	 * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 	 */
-	
+
 	let isPaused = true;
 	let frameRate = 60;
 
 	const numberOfPixelsInGrid = 20;
 	const pixelSize = 20;
 	const gridSize = numberOfPixelsInGrid * pixelSize;
-	
+
 	const gridCells: Cell[][] = initCellState();
 
 	function initCellState(): Cell[][] {
 		const initialArray: Cell[][] = [];
 
-		for(let i = 0; i < numberOfPixelsInGrid; i++){
+		for (let i = 0; i < numberOfPixelsInGrid; i++) {
 			initialArray[i] = [];
-			for(let j = 0; j < numberOfPixelsInGrid; j++) {
-				initialArray[i].push(new Cell(i * pixelSize, j * pixelSize, false, pixelSize));
+			for (let j = 0; j < numberOfPixelsInGrid; j++) {
+				initialArray[i].push(
+					new Cell(i * pixelSize, j * pixelSize, false, pixelSize),
+				);
 			}
 		}
 
@@ -40,19 +42,18 @@
 	function handleResetClicked() {
 		isPaused = true;
 
-		for(let i = 0; i < gridCells.length; i++){
-			for(let j = 0; j < gridCells.length; j++) {
+		for (let i = 0; i < gridCells.length; i++) {
+			for (let j = 0; j < gridCells.length; j++) {
 				gridCells[i][j].kill();
 			}
 		}
 	}
-	
-	
+
 	function handleRandomiseClicked() {
 		isPaused = true;
 
-		for(let i = 0; i < gridCells.length; i++){
-			for(let j = 0; j < gridCells.length; j++) {
+		for (let i = 0; i < gridCells.length; i++) {
+			for (let j = 0; j < gridCells.length; j++) {
 				if (Math.random() > 0.5) {
 					gridCells[i][j].spawn();
 				} else {
@@ -63,11 +64,10 @@
 	}
 
 	function processTick() {
-		gridCells.forEach((cellRow, i) => {
-			cellRow.forEach((cell, j) => {
+		for (let i = 0; i < gridCells.length; i++) {
+			for (let j = 0; j < gridCells.length; j++) {
 				const currentCell = gridCells[i][j];
-				
-				//Get each neighbour
+
 				const northWestNeighbour = gridCells[i + 1]?.[j - 1];
 				const northNeighbour = gridCells[i + 1]?.[j];
 				const northEastNeighbour = gridCells[i + 1]?.[j + 1];
@@ -75,11 +75,10 @@
 				const southWestNeighbour = gridCells[i - 1]?.[j - 1];
 				const southNeighbour = gridCells[i - 1]?.[j];
 				const southEastNeighbour = gridCells[i - 1]?.[j + 1];
-				
+
 				const eastNeighbour = gridCells[i]?.[j + 1];
 				const westNeighbour = gridCells[i]?.[j - 1];
 
-				//Generate counts from them
 				const numberOfNeighbours = [
 					northNeighbour,
 					southNeighbour,
@@ -88,20 +87,22 @@
 					northWestNeighbour,
 					northEastNeighbour,
 					southWestNeighbour,
-					southEastNeighbour
-				].filter((neighbour) => !!neighbour && neighbour.isAlive).length;
-				
-				currentCell.loadNextMove(numberOfNeighbours);
-			})
-		})
+					southEastNeighbour,
+				].filter(
+					(neighbour) => !!neighbour && neighbour.isAlive,
+				).length;
 
-		gridCells.forEach((cellRow, i) => {
-			cellRow.forEach((cell, j) => {
+				currentCell.loadNextMove(numberOfNeighbours);
+			}
+		}
+
+		for (let i = 0; i < gridCells.length; i++) {
+			for (let j = 0; j < gridCells.length; j++) {
 				const currentCell = gridCells[i][j];
-				
+
 				currentCell.executeNextMove();
-			})
-		})
+			}
+		}
 	}
 
 	const sketch: Sketch = (p5: p5) => {
@@ -109,8 +110,10 @@
 			p5.createCanvas(gridSize, gridSize);
 			p5.background(255);
 
-			gridCells.forEach((cellRow) => {
-				cellRow.forEach((cell) => {
+			for (let i = 0; i < gridCells.length; i++) {
+				for (let j = 0; j < gridCells.length; j++) {
+					const cell = gridCells[i][j];
+
 					p5.push();
 					if (cell.isAlive) {
 						p5.fill(0);
@@ -120,19 +123,20 @@
 						p5.square(cell.x, cell.y, pixelSize);
 					}
 					p5.pop();
-				})
-			})
+				}
+			}
 		};
 
 		p5.draw = () => {
 			p5.frameRate(frameRate);
-			
+
 			if (!isPaused) {
 				processTick();
 			}
 
-			gridCells.forEach((cellRow) => {
-				cellRow.forEach((cell) => {
+			for (let i = 0; i < gridCells.length; i++) {
+				for (let j = 0; j < gridCells.length; j++) {
+					const cell = gridCells[i][j];
 					p5.push();
 					if (cell.isAlive) {
 						p5.fill(0);
@@ -142,8 +146,8 @@
 						p5.square(cell.x, cell.y, pixelSize);
 					}
 					p5.pop();
-				})
-			})
+				}
+			}
 		};
 
 		p5.mouseClicked = () => {
@@ -156,17 +160,17 @@
 			if (mouseY < 0 || mouseY > gridSize) {
 				return;
 			}
-			
-			gridCells.forEach((row) => {
-				const cellClicked = row.find((cell) => cell.hasBeenClicked(mouseX, mouseY))
 
-				if (!cellClicked) {
-					return;
+			for (let i = 0; i < gridCells.length; i++) {
+				for (let j = 0; j < gridCells.length; j++) {
+					const cell = gridCells[i][j];
+
+					if (cell.hasBeenClicked(mouseX, mouseY)) {
+						cell.isAlive ? cell.kill() : cell.spawn();
+					}
 				}
-
-				cellClicked.isAlive ? cellClicked.kill() : cellClicked.spawn();
-			})
-		}
+			}
+		};
 	};
 </script>
 

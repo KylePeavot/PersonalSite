c00b3bf063a187356b309746fe978d1229c3b674
Kyle Peavot <kyle.peavot@ebm.ai>
2024-02-05 16:11:52 +0000

    Update page content with additional information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
 <h1>Kyle Peavot</h1>
 <h2>Full Stack Software Engineer</h2>
 
+<hr />
+
 <a href="/p5">Here's</a> some projects using the <a href="https://p5js.org/">p5.js</a> library
+
+<hr />
+
+<p>
+    <a href="https://berries.peavot.com">Here's</a> an imaginary store front that allows the customer to purchase* berries from Professor Oak and then enter delivery details for the berries to be delivered to**. 
+</p>
+
+<i>*No financial information is collected or required</i>
+
+<i>**Berries may or may not be delivered</i>
+
+<hr />

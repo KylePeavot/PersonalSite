6a0562cace8788af8ee9d98a539833d608a9253c
Kyle Peavot <kyle.peavot@ebm.ai>
2024-07-29 07:11:17 +0100

    Hotjar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
     import "../app.css";
     import { Sidebar } from "$lib/common/components/index";
+    import { onMount } from "svelte";
+    import Hotjar from "@hotjar/browser";
+
+    onMount(() => {
+        const siteId = 3878067;
+        const hotjarVersion = 6;
+
+        Hotjar.init(siteId, hotjarVersion);
+    });
 </script>
 
 <div class="flex flex-col bg-slate-100 w-full h-full">

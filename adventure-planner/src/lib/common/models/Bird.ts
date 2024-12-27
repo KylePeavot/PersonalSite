49a9aced285a68a6f6d153cab919e1d9ddab8d0a
Kyle Peavot <kyle.peavot@ebm.ai>
2024-12-27 15:22:54 +0000

    added velocity and acceleration to birds

--- adventure-planner/src/lib/common/models/Bird.ts
+++ adventure-planner/src/lib/common/models/Bird.ts
@@ -1,13 +1,15 @@
 export class Bird {
     public x: number;
     public y: number;
-    public acceleration: number;
-    public velocity: number;
+    public vx: number;
+    public vy: number;
+    public direction: number; //in radians
 
-    constructor(x: number, y: number, acceleration: number, velocity: number) {
+    constructor(x: number, y: number, vx: number, vy: number, direction: number) {
         this.x = x;
         this.y = y;
-        this.acceleration = acceleration;
-        this.velocity = velocity;
+        this.vx = vx;
+        this.vy = vy;
+        this.direction = direction;
     }
 }
